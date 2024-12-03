9ef95b23f610549830db40ad19f4a3a978181afd
Gustavo Salmazo <gux.salmazo@gmail.com>
2024-12-03 12:51:27 -0300

    routes + params

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express")
 const app = express()
 
+app.get("/", function(req,ans){
+    ans.send("Welcome to my app!")
+})
+
+app.get("/about", function(req,ans){
+    ans.send("Read about my app!")
+})
+
+app.get("/second_page", function(req,ans){
+    ans.send("thats second page!")
+})
+
+app.get("/ola/:cargo/:empresa/:nome", function(req,ans){
+    ans.send("<h1>Ola " + req.params.nome + "</h1>" + "<h2>seu cargo e:" + req.params.cargo + "</h2>" + "<h3>e vc trabalha na empresa: " + req.params.empresa + "</h3>")
+})
 
 
 
-
-app.listen(7071)
+app.listen(8081, function(){
+    console.log("Server is Running on http://localhost:8081")
+})
